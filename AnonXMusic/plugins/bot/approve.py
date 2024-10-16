2f6752e6b1cf02331c25e186d9dfc63f700676d3
𝐊ʀɪsʜ 𝐌ɪsʜʀᴀ <vimal9559459031@gmail.com>
2024-10-16 16:24:25 +0530

    Update approve.py
    Signed-off-by: 𝐊ʀɪsʜ 𝐌ɪsʜʀᴀ  <vimal9559459031@gmail.com>

--- AnonXMusic/plugins/bot/approve.py
+++ AnonXMusic/plugins/bot/approve.py
@@ -1,6 +1,5 @@
 from AnonXMusic import app
 from os import environ
-from config import BOT_USERNAME
 import config
 from pyrogram import Client, filters
 from pyrogram.types import ChatJoinRequest, InlineKeyboardButton, InlineKeyboardMarkup
