99429fe66759a3d736d1677dfc74f406c3c98837
𝐊ʀɪsʜ 𝐌ɪsʜʀᴀ <vimal9559459031@gmail.com>
2024-10-16 13:07:24 +0530

    Update config.py
    Signed-off-by: 𝐊ʀɪsʜ 𝐌ɪsʜʀᴀ  <vimal9559459031@gmail.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Xkrishmishra/MISHXV2",
+    "https://github.com/Krishnetwork/Restart-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
